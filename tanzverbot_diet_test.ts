546d362ced09cdcb4140486af476fbd6fb62ce79
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-22 08:47:34 +0200

    add test for woman

--- tanzverbot_diet_test.ts
+++ tanzverbot_diet_test.ts
@@ -4,3 +4,7 @@ import { calcDateOnDiet, Sex } from "./tanzverbot_diet.ts";
 Deno.test("Tanzverbot Diet", () => {
   assertGreater(calcDateOnDiet(74, 100, 1.86, 38, Sex.Male), 0.0);
 });
+
+Deno.test("Tanzverbot Diet", () => {
+  assertGreater(calcDateOnDiet(74, 100, 1.86, 38, Sex.Female), 0.0);
+});
